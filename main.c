f3aa53a26aa2adc04f4403af862b5b0dc346c2ab
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-29 12:41:58 -0400

    deletes refactored code [clang]
    COMMENTS:
    Deletes the code that has been already refactored into functions.
    This referes to the code enclosed by multi-line comments /* ... */.
    
    Hopefully this approach improves the git history.

--- main.c
+++ main.c
@@ -357,26 +357,14 @@ void loadFromFile (cartridge_t* c)
 
   // reads ROM header:
 
-  /*
-  size_t size = 0x10;
-  byte header[size];
-  size_t count = fread(header, sizeof(byte), size, rom);
-  if (count != size)
-  {
-    printf("Invalid NES ROM\n");
-    fclose(rom);
-    return;
-  }
-
-  printf("header: %c %c %c %x\n", header[0], header[1], header[2], header[3]);
-  */
-
   int stat;
   stat = load_H_ROM(rom, c);
   if (stat != 0)
   {
     return;
   }
+
+  // reads ROM data:
 
   stat = hasTrainerSupport(rom, c);
   if (stat != 0)
@@ -400,152 +388,6 @@ void loadFromFile (cartridge_t* c)
   setMapperNumber(c);
   setExtendedRAM(c);
   info_colorSystem(c);
-
-  /*
-  byte banks = header[4];
-  printf("16KB PRG-ROM Banks: %u \n", banks);
-  if (!banks)
-  {
-    printf("16KB PRG-ROM Banks: %u \n", banks);
-    printf("ROM does not have PRG-ROM Banks! Failed to load ROM\n");
-    fclose(rom);
-    return;
-  }
-  */
-
-  // reads ROM data:
-
-  /*
-  byte vbanks = header[5];
-  printf("8KB CHR-ROM Banks: %u \n", vbanks);
-  */
-
-  /*
-  if (header[6] & 0x8)
-  {
-    enum nameTableMirroring mirroring;
-    mirroring = FourScreen;
-    byte m_nameTableMirroring = mirroring;
-    printf("Name Table Mirroring: %u \n", m_nameTableMirroring);
-    c -> m_nameTableMirroring = m_nameTableMirroring;
-  }
-  else
-  {
-    byte m_nameTableMirroring = (header[6] & 0x1);
-    switch (m_nameTableMirroring)
-    {
-      case 0:
-	printf("Name Table Mirroring: Horizontal\n");
-	break;
-      case 1:
-	printf("Name Table Mirroring: Vertical\n");
-	break;
-      default:
-	printf("Name Table Mirroring: Unknown\n");
-	break;
-    }
-    c -> m_nameTableMirroring = m_nameTableMirroring;
-  }
-  */
-
-  /*
-  byte m_mapperNumber = ((header[6] >> 4) & 0xf) | (header[7] & 0xf0);
-  c -> m_mapperNumber = m_mapperNumber;
-  printf("Mapper Number: %u \n", m_mapperNumber);
-  */
-
-  /*
-  bool m_extendedRAM = (header[6] & 0x2)? true : false;
-  c -> m_extendedRAM = m_extendedRAM;
-  printf("Extended CPU RAM: %u \n", m_extendedRAM);
-  */
-
-  /*
-  if (header[6] & 0x4)
-  {
-    printf("Unsupported Trainer!\n");
-    fclose(rom);
-    return;
-  }
-  */
-
-  /*
-  //if ( (header[0xa] & 0x3) == 0x2 || (header[0xa] & 0x1) ) // what you should test
-  if ( (header[0xa] & 0x1) )
-  {
-    // NOTE: dunno why the ROM is not PAL compatible and how serious that really is
-    printf("Unsupported PAL ROM!\n");
-    fclose(rom);
-    return;
-  }
-  else
-  {
-    printf("ROM is NTSC compatible\n");
-  }
-  */
-
-  /*
-  size_t num_banks = (0x4000 * banks);
-  byte b_PRG_ROM[num_banks];
-  count = fread(b_PRG_ROM, sizeof(byte), num_banks, rom);
-  if (num_banks != count)
-  {
-    printf("Failed to read PRG-ROM!\n");
-    fclose(rom);
-    return;
-  }
-
-  c -> m_PRG_ROM = (byte*) malloc( num_banks * sizeof(byte) );
-
-  if (c -> m_PRG_ROM == NULL)
-  {
-    printf("failed to allocate memory for PRG-ROM!\n");
-    fclose(rom);
-    return;
-  }
-
-  byte* m_PRG_ROM = c -> m_PRG_ROM;
-  for (size_t i = 0; i != num_banks; ++i)
-  {
-    m_PRG_ROM[i] = b_PRG_ROM[i];
-  }
-  */
-
-  /*
-  if (vbanks)
-  {
-    size_t num_vbanks = (0x2000 * vbanks);
-    byte b_CHR_ROM[num_vbanks];
-    count = fread(b_CHR_ROM, sizeof(byte), num_vbanks, rom);
-    if (num_vbanks != count)
-    {
-      printf("Failed to read CHR-ROM!\n");
-      free(c -> m_PRG_ROM);
-      c -> m_PRG_ROM = NULL;
-      fclose(rom);
-      return;
-    }
-
-    c -> m_CHR_ROM = (byte*) malloc( num_vbanks * sizeof(byte) );
-
-    if (c -> m_CHR_ROM == NULL)
-    {
-      printf("failed to allocate memory for CHR-ROM!\n");
-      free(c -> m_PRG_ROM);
-      c -> m_PRG_ROM = NULL;
-      fclose(rom);
-      return;
-    }
-
-    byte* m_CHR_ROM = c -> m_CHR_ROM;
-    for (size_t i = 0; i != num_vbanks; ++i)
-    {
-      m_CHR_ROM[i] = b_CHR_ROM[i];
-    }
-  }
-
-  printf("ROM with CHR-RAM\n");
-  */
 
   fclose(rom);
 }
